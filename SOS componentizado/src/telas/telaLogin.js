d3c2f541ad78b94683ec4830eb017ac66f3155de
GratzJulia <julia.gratz@aluno.cefet-rj.br>
2021-04-01 18:40:14 -0300

    Validacao de Form
    Validacoes para o Form de Login utilizando biblioteca Yup.

--- SOS componentizado/src/telas/telaLogin.js
+++ SOS componentizado/src/telas/telaLogin.js
@@ -1,20 +1,58 @@
 import React from "react";
 import "../estilos/style.css";
+import * as yup from "yup";
+import { ptForm } from "yup-locale-pt";
+import { Formik, Form as FF, Field, ErrorMessage } from "formik";
 
-const Login = () => {
-  return (
-    <div className="flexible">
-      <form method="POST">
-        <input type="text" placeholder="Digite seu email aqui" />
-        <input type="password" id="senha" placeholder="Digite a senha aqui" />
-        <input
-          className="btn btn-primary"
-          id="entrar"
-          type="submit"
-          value="Entrar"
-        />
-      </form>
-    </div>
-  );
-};
+yup.setLocale(ptForm); //validacoes em portugues!
+
+const schemaValidacoes = yup.object().shape({
+  usuario: yup
+    .string()
+    .email()
+    .required(),
+  senha: yup
+    .string()
+    .min(6)
+    .required()
+});
+
+const obj = { usuario: "", senha: "" };
+
+function enviar(event) {
+  event.preventDefault();
+  alert("Logando... Aguarde.");
+}
+
+const Login = () => (
+  <Formik
+    initialValues={obj}
+    onSubmit={enviar}
+    validationSchema={schemaValidacoes}
+  >
+    <FF className="Form">
+      <Field
+        className="Form-Field"
+        type="text"
+        name="usuario"
+        placeholder="Digite seu email aqui"
+      />
+      <ErrorMessage component="span" name="usuario" />
+      <Field
+        className="Form-Field"
+        type="password"
+        name="senha"
+        placeholder="Digite sua senha aqui"
+      />
+      <ErrorMessage component="span" name="senha" />
+      <input
+        className="btn btn-primary"
+        id="entrar"
+        type="submit"
+        value="Entrar"
+      />
+    </FF>
+  </Formik>
+);
+
 export default Login;
